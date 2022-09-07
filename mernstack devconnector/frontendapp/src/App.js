79bfb859f0f50da508460e347d6eed440eeff513
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-09-07 09:28:35 -0700

    frontend app

---
+++ mernstack devconnector/frontendapp/src/App.js
@@ -0,0 +1,29 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+
+const App = () => 
+(
+ <Router>
+ <Fragment>
+ 
+  <Navbar />
+  <Route exact path='/' component={Landing} />
+ 
+ </Fragment > 
+ </Router>
+
+);
+
+
+
+
+
+
+
+
+export default App;
