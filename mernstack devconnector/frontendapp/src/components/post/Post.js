febc49d0d0526b30eaef29686dc5b28763a62219
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-11-02 12:25:29 -0700

    frontend app

--- mernstack devconnector/frontendapp/src/components/post/Post.js
+++ mernstack devconnector/frontendapp/src/components/post/Post.js
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../action/post';
 import { Link } from 'react-router-dom';
 
@@ -14,8 +16,14 @@ const Post = ({ getPost, post: { post, loading }, match}) => {
         Back to Posts
     </Link>
     <PostItem post={post} showActions={false} />
+    <CommentForm postId={post._id} />
+    <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
   </Fragment>
-}
+};
 
 
 
--- mernstack devconnector/frontendapp/src/reducers/post.js
+++ mernstack devconnector/frontendapp/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from '../action/types';
 
 const initialState = {
@@ -55,6 +57,23 @@ function postReducer(state = initialState, action) {
           ),
           loading: false
         };
+        case ADD_COMMENT:
+          return {
+            ...state,
+            post: { ...state.post, comments: payload },
+            loading: false
+          };
+        case REMOVE_COMMENT:
+          return {
+            ...state,
+            post: {
+              ...state.post,
+              comments: state.post.comments.filter(
+                (comment) => comment._id !== payload
+              )
+            },
+            loading: false
+          };
       default:
         return state;
     }
